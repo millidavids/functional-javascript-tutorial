ca0f12512956bdd63cdb051721b5e4871ff8fa79
David Yurek <david.a.yurek@gmail.com>
2015-09-24 10:01:22 -0400

    basic recursion

--- program.js
+++ program.js
@@ -1,8 +1,8 @@
-function countWords(inputWords) {
-    return inputWords.reduce(function(hash, word){
-        hash[word] = ++hash[word] || 1;
-        return hash;
-    }, {});
+function reduce(arr, fn, initial) {
+    return (function reduceOne(index, value) {
+        if(index > arr.length - 1) return value;
+        return reduceOne(index+1, fn(value, arr[index], index, arr));
+    })(0, initial);
 }
 
-module.exports = countWords; 
+module.exports = reduce; 
