00e793d179bb228af789639f5aecb23c331f751b
David Yurek <david.a.yurek@gmail.com>
2015-09-25 07:52:01 -0400

    basic call

--- program.js
+++ program.js
@@ -1,8 +1,7 @@
-function reduce(arr, fn, initial) {
-    return (function reduceOne(index, value) {
-        if(index > arr.length - 1) return value;
-        return reduceOne(index+1, fn(value, arr[index], index, arr));
-    })(0, initial);
+function duckCount() {
+    return Array.prototype.slice.call(arguments).filter(function(obj){
+        return Object.prototype.hasOwnProperty.call(obj, 'quack');
+    }).length;
 }
 
-module.exports = reduce; 
+module.exports = duckCount; 
