a261b84a8f493a9d78dc0e7f5b6f8bcd4555d163
David Yurek <david.a.yurek@gmail.com>
2015-09-26 09:56:13 -0400

    trampoline

--- program.js
+++ program.js
@@ -1,14 +1,19 @@
 function repeat(operation, num) {
+  return function() {
     if (num <= 0) return;
     operation();
-    
-    if (num % 10 === 0) {
-        setTimeout(function() {
-            repeat(operation, --num);
-        })
-    } else {
-        repeat(operation, --num);
-    }
+    return repeat(operation, --num);
+  };
 }
 
-module.exports = repeat;
+function trampoline(fn) {
+  while(fn && typeof fn === 'function') {
+    fn = fn();
+  }
+}
+
+module.exports = function(operation, num) {
+  trampoline(function() {
+    return repeat(operation, num)
+  });
+};
