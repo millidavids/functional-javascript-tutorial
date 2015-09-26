e5b945f215e7d9af56726133e491f35c8126ae0c
David Yurek <david.a.yurek@gmail.com>
2015-09-26 09:36:03 -0400

    implement map with reduce

--- program.js
+++ program.js
@@ -1,3 +1,6 @@
-module.exports = function(namespace) {
-    return console.log.bind(console, namespace);
+module.exports = function arrayMap(arr, fn, thisArg) {
+    return arr.reduce(function(previous, current, index, array) {
+        previous.push(fn.call(thisArg, current, index, array));
+        return previous;
+    }, []);
 }
